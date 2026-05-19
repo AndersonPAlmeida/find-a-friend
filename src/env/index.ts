4def31146c023e4472abfc69c2b8d7b661578387
Anderson Almeida <andersonpa10pa@gmail.com>
2026-05-18 21:27:47 -0300

    Configuração do lint Biome

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,14 +3,17 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
-  console.error('❌ Variáveis ​​de ambiente inválidas.', z.treeifyError(_env.error));
-  
+if (_env.success === false) {
+  console.error(
+    '❌ Variáveis ​​de ambiente inválidas.',
+    z.treeifyError(_env.error)
+  )
+
   throw new Error('Variáveis ​​de ambiente inválidas.')
 }
 
