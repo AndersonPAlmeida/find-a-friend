a3d25fde14f5b956cda382b5c7026b6303195bc2
Anderson Almeida <andersonpa10pa@gmail.com>
2026-05-19 17:41:14 -0300

    Configuração e teste do Prisma Client

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
 import fastify from 'fastify'
+import { prisma } from './lib/prisma.js'
 
 export const app = fastify()
+
+app.post('/', async (request, reply) => {
+  await prisma.orgs.create({
+    data: {
+      cep: '0000000',
+      cidade: 'jequie',
+      email: 'ander@gmail.com',
+      nome: 'anderson',
+      endereco_rua: 'adasdad',
+      estado: 'ba',
+      password_hash: '123456',
+      whatsapp: '7399915',
+    },
+  })
+
+  return reply.status(201).send()
+})
