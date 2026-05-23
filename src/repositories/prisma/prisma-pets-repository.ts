0060e98be4b70394947c125a07bd73969e3b00bc
Anderson Almeida <andersonpa10pa@gmail.com>
2026-05-23 15:50:15 -0300

    Implementação dos metodos no repositorio do Prisma da tabela de Pets.

--- src/repositories/prisma/prisma-pets-repository.ts
+++ src/repositories/prisma/prisma-pets-repository.ts
@@ -1,9 +1,72 @@
 import { prisma } from '@/lib/prisma'
-import type { Pet, Prisma } from '@/lib/prisma/generated/prisma/client'
+import type {
+  Ambiente,
+  Idade,
+  Independencia,
+  Nivel_Energia,
+  Prisma,
+  Tamanho,
+} from '@/lib/prisma/generated/prisma/client'
 import type { PetsRepository } from '../pets-repository'
 
 export class PrismaPetsRepository implements PetsRepository {
-  create(data: Prisma.PetUncheckedCreateInput): Promise<Pet> {
-    throw new Error('Method not implemented.')
+  async findById(idPet: string) {
+    const pet = await prisma.pet.findUnique({
+      where: {
+        id: idPet,
+      },
+    })
+
+    return pet
+  }
+
+  async findManyPets(
+    cityOrg: string,
+    page: number,
+    idade?: Idade,
+    tamanho?: Tamanho,
+    nivelEnergia?: Nivel_Energia,
+    independencia?: Independencia,
+    ambiente?: Ambiente
+  ) {
+    const where: Prisma.PetWhereInput = {
+      cidade_org: cityOrg,
+    }
+
+    if (idade) {
+      where.idade = idade
+    }
+
+    if (tamanho) {
+      where.tamanho = tamanho
+    }
+
+    if (nivelEnergia) {
+      where.nivel_energia = nivelEnergia
+    }
+
+    if (independencia) {
+      where.independencia = independencia
+    }
+
+    if (ambiente) {
+      where.ambiente = ambiente
+    }
+
+    const pets = await prisma.pet.findMany({
+      where,
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return pets
+  }
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = await prisma.pet.create({
+      data,
+    })
+
+    return pet
   }
 }
